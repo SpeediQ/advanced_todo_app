21051077d99e3a9f7e3da2bb07e000f93c2ee3d7
Marcin Kowalczyk <kowalczyk.m@int.pl>
2021-03-10 22:47:31 +0100

    flywaydb added to dependencies. TODO: add migration

--- src/main/java/pl/kowalczyk/advanced_todo_app/controller/InfoController.java
+++ src/main/java/pl/kowalczyk/advanced_todo_app/controller/InfoController.java
@@ -1,5 +1,6 @@
 package pl.kowalczyk.advanced_todo_app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ public class InfoController {
     private DataSourceProperties dataSource;
     private TaskConfigurationProperties myProp;
 
+    @Autowired
     public InfoController(final DataSourceProperties dataSource, final TaskConfigurationProperties myProp) {
         this.dataSource = dataSource;
         this.myProp = myProp;
@@ -22,7 +24,7 @@ public class InfoController {
 
     @GetMapping("/info/prop")
     boolean myProp() {
-        return myProp.isAllowMultipleTasksFromTemplate();
+        return myProp.getTemplate().isAllowMultipleTasks();
     }
 
 }
