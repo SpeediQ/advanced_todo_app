427a09dc51c628852661453d4ba8ea80c1c34ca7
Marcin Kowalczyk <kowalczyk.m@int.pl>
2021-03-19 20:46:54 +0100

    Added Services and projections

--- src/main/java/oi/kowalczyk/advanced_todo_app/adapter/SqlProjectRepository.java
+++ src/main/java/oi/kowalczyk/advanced_todo_app/adapter/SqlProjectRepository.java
@@ -1,5 +1,7 @@
 package oi.kowalczyk.advanced_todo_app.adapter;
 
+import oi.kowalczyk.advanced_todo_app.model.Project;
+import oi.kowalczyk.advanced_todo_app.model.ProjectRepository;
 import oi.kowalczyk.advanced_todo_app.model.TaskGroup;
 import oi.kowalczyk.advanced_todo_app.model.TaskGroupRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,12 +11,9 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
+public interface SqlProjectRepository extends ProjectRepository, JpaRepository<Project, Integer> {
 
     @Override
-    @Query("select distinct g from TaskGroup g join fetch g.tasks")
-    List<TaskGroup> findAll();
-
-    @Override
-    boolean existsByDoneIsFalseAndProject_Id(Integer projectId);
+    @Query("select distinct p from Project p join fetch p.steps")
+    List<Project> findAll();
 }
--- src/main/resources/db/migration/V5__init_task_group_table.sql
+++ src/main/resources/db/migration/V5__init_task_group_table.sql
@@ -1,12 +1,10 @@
-create table task_group
+create table task_groups
 (
     id          int primary key auto_increment,
     description varchar(100) not null,
     done        bit
 );
+alter table tasks add column task_group_id int null;
+alter table tasks
+    add foreign key (task_group_id) references task_groups (id);
 
-alter table TASKS
-    add column task_group_id int null;
-
-alter table TASKS
-    add foreign key (task_group_id) references task_group (id);
